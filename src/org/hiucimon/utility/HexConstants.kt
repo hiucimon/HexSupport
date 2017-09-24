6e2ad1f7082ecaddc72b938f33eee94eb5dbd0a8
joe <Joe.Bryant@me.com>
2017-09-23 22:30:46 -0400

    Initial commit

---
+++ src/org/hiucimon/utility/HexConstants.kt
@@ -0,0 +1,524 @@
+package org.hiucimon.utility
+
+const val X00 = 0x0000.toInt()
+const val X01 = 0x0001.toInt()
+const val X02 = 0x0002.toInt()
+const val X03 = 0x0003.toInt()
+const val X04 = 0x0004.toInt()
+const val X05 = 0x0005.toInt()
+const val X06 = 0x0006.toInt()
+const val X07 = 0x0007.toInt()
+const val X08 = 0x0008.toInt()
+const val X09 = 0x0009.toInt()
+const val X0a = 0x000a.toInt()
+const val X0b = 0x000b.toInt()
+const val X0c = 0x000c.toInt()
+const val X0d = 0x000d.toInt()
+const val X0e = 0x000e.toInt()
+const val X0f = 0x000f.toInt()
+const val X10 = 0x0010.toInt()
+const val X11 = 0x0011.toInt()
+const val X12 = 0x0012.toInt()
+const val X13 = 0x0013.toInt()
+const val X14 = 0x0014.toInt()
+const val X15 = 0x0015.toInt()
+const val X16 = 0x0016.toInt()
+const val X17 = 0x0017.toInt()
+const val X18 = 0x0018.toInt()
+const val X19 = 0x0019.toInt()
+const val X1a = 0x001a.toInt()
+const val X1b = 0x001b.toInt()
+const val X1c = 0x001c.toInt()
+const val X1d = 0x001d.toInt()
+const val X1e = 0x001e.toInt()
+const val X1f = 0x001f.toInt()
+const val X20 = 0x0020.toInt()
+const val X21 = 0x0021.toInt()
+const val X22 = 0x0022.toInt()
+const val X23 = 0x0023.toInt()
+const val X24 = 0x0024.toInt()
+const val X25 = 0x0025.toInt()
+const val X26 = 0x0026.toInt()
+const val X27 = 0x0027.toInt()
+const val X28 = 0x0028.toInt()
+const val X29 = 0x0029.toInt()
+const val X2a = 0x002a.toInt()
+const val X2b = 0x002b.toInt()
+const val X2c = 0x002c.toInt()
+const val X2d = 0x002d.toInt()
+const val X2e = 0x002e.toInt()
+const val X2f = 0x002f.toInt()
+const val X30 = 0x0030.toInt()
+const val X31 = 0x0031.toInt()
+const val X32 = 0x0032.toInt()
+const val X33 = 0x0033.toInt()
+const val X34 = 0x0034.toInt()
+const val X35 = 0x0035.toInt()
+const val X36 = 0x0036.toInt()
+const val X37 = 0x0037.toInt()
+const val X38 = 0x0038.toInt()
+const val X39 = 0x0039.toInt()
+const val X3a = 0x003a.toInt()
+const val X3b = 0x003b.toInt()
+const val X3c = 0x003c.toInt()
+const val X3d = 0x003d.toInt()
+const val X3e = 0x003e.toInt()
+const val X3f = 0x003f.toInt()
+const val X40 = 0x0040.toInt()
+const val X41 = 0x0041.toInt()
+const val X42 = 0x0042.toInt()
+const val X43 = 0x0043.toInt()
+const val X44 = 0x0044.toInt()
+const val X45 = 0x0045.toInt()
+const val X46 = 0x0046.toInt()
+const val X47 = 0x0047.toInt()
+const val X48 = 0x0048.toInt()
+const val X49 = 0x0049.toInt()
+const val X4a = 0x004a.toInt()
+const val X4b = 0x004b.toInt()
+const val X4c = 0x004c.toInt()
+const val X4d = 0x004d.toInt()
+const val X4e = 0x004e.toInt()
+const val X4f = 0x004f.toInt()
+const val X50 = 0x0050.toInt()
+const val X51 = 0x0051.toInt()
+const val X52 = 0x0052.toInt()
+const val X53 = 0x0053.toInt()
+const val X54 = 0x0054.toInt()
+const val X55 = 0x0055.toInt()
+const val X56 = 0x0056.toInt()
+const val X57 = 0x0057.toInt()
+const val X58 = 0x0058.toInt()
+const val X59 = 0x0059.toInt()
+const val X5a = 0x005a.toInt()
+const val X5b = 0x005b.toInt()
+const val X5c = 0x005c.toInt()
+const val X5d = 0x005d.toInt()
+const val X5e = 0x005e.toInt()
+const val X5f = 0x005f.toInt()
+const val X60 = 0x0060.toInt()
+const val X61 = 0x0061.toInt()
+const val X62 = 0x0062.toInt()
+const val X63 = 0x0063.toInt()
+const val X64 = 0x0064.toInt()
+const val X65 = 0x0065.toInt()
+const val X66 = 0x0066.toInt()
+const val X67 = 0x0067.toInt()
+const val X68 = 0x0068.toInt()
+const val X69 = 0x0069.toInt()
+const val X6a = 0x006a.toInt()
+const val X6b = 0x006b.toInt()
+const val X6c = 0x006c.toInt()
+const val X6d = 0x006d.toInt()
+const val X6e = 0x006e.toInt()
+const val X6f = 0x006f.toInt()
+const val X70 = 0x0070.toInt()
+const val X71 = 0x0071.toInt()
+const val X72 = 0x0072.toInt()
+const val X73 = 0x0073.toInt()
+const val X74 = 0x0074.toInt()
+const val X75 = 0x0075.toInt()
+const val X76 = 0x0076.toInt()
+const val X77 = 0x0077.toInt()
+const val X78 = 0x0078.toInt()
+const val X79 = 0x0079.toInt()
+const val X7a = 0x007a.toInt()
+const val X7b = 0x007b.toInt()
+const val X7c = 0x007c.toInt()
+const val X7d = 0x007d.toInt()
+const val X7e = 0x007e.toInt()
+const val X7f = 0x007f.toInt()
+const val X80 = 0x0080.toInt()
+const val X81 = 0x0081.toInt()
+const val X82 = 0x0082.toInt()
+const val X83 = 0x0083.toInt()
+const val X84 = 0x0084.toInt()
+const val X85 = 0x0085.toInt()
+const val X86 = 0x0086.toInt()
+const val X87 = 0x0087.toInt()
+const val X88 = 0x0088.toInt()
+const val X89 = 0x0089.toInt()
+const val X8a = 0x008a.toInt()
+const val X8b = 0x008b.toInt()
+const val X8c = 0x008c.toInt()
+const val X8d = 0x008d.toInt()
+const val X8e = 0x008e.toInt()
+const val X8f = 0x008f.toInt()
+const val X90 = 0x0090.toInt()
+const val X91 = 0x0091.toInt()
+const val X92 = 0x0092.toInt()
+const val X93 = 0x0093.toInt()
+const val X94 = 0x0094.toInt()
+const val X95 = 0x0095.toInt()
+const val X96 = 0x0096.toInt()
+const val X97 = 0x0097.toInt()
+const val X98 = 0x0098.toInt()
+const val X99 = 0x0099.toInt()
+const val X9a = 0x009a.toInt()
+const val X9b = 0x009b.toInt()
+const val X9c = 0x009c.toInt()
+const val X9d = 0x009d.toInt()
+const val X9e = 0x009e.toInt()
+const val X9f = 0x009f.toInt()
+const val Xa0 = 0x00a0.toInt()
+const val Xa1 = 0x00a1.toInt()
+const val Xa2 = 0x00a2.toInt()
+const val Xa3 = 0x00a3.toInt()
+const val Xa4 = 0x00a4.toInt()
+const val Xa5 = 0x00a5.toInt()
+const val Xa6 = 0x00a6.toInt()
+const val Xa7 = 0x00a7.toInt()
+const val Xa8 = 0x00a8.toInt()
+const val Xa9 = 0x00a9.toInt()
+const val Xaa = 0x00aa.toInt()
+const val Xab = 0x00ab.toInt()
+const val Xac = 0x00ac.toInt()
+const val Xad = 0x00ad.toInt()
+const val Xae = 0x00ae.toInt()
+const val Xaf = 0x00af.toInt()
+const val Xb0 = 0x00b0.toInt()
+const val Xb1 = 0x00b1.toInt()
+const val Xb2 = 0x00b2.toInt()
+const val Xb3 = 0x00b3.toInt()
+const val Xb4 = 0x00b4.toInt()
+const val Xb5 = 0x00b5.toInt()
+const val Xb6 = 0x00b6.toInt()
+const val Xb7 = 0x00b7.toInt()
+const val Xb8 = 0x00b8.toInt()
+const val Xb9 = 0x00b9.toInt()
+const val Xba = 0x00ba.toInt()
+const val Xbb = 0x00bb.toInt()
+const val Xbc = 0x00bc.toInt()
+const val Xbd = 0x00bd.toInt()
+const val Xbe = 0x00be.toInt()
+const val Xbf = 0x00bf.toInt()
+const val Xc0 = 0x00c0.toInt()
+const val Xc1 = 0x00c1.toInt()
+const val Xc2 = 0x00c2.toInt()
+const val Xc3 = 0x00c3.toInt()
+const val Xc4 = 0x00c4.toInt()
+const val Xc5 = 0x00c5.toInt()
+const val Xc6 = 0x00c6.toInt()
+const val Xc7 = 0x00c7.toInt()
+const val Xc8 = 0x00c8.toInt()
+const val Xc9 = 0x00c9.toInt()
+const val Xca = 0x00ca.toInt()
+const val Xcb = 0x00cb.toInt()
+const val Xcc = 0x00cc.toInt()
+const val Xcd = 0x00cd.toInt()
+const val Xce = 0x00ce.toInt()
+const val Xcf = 0x00cf.toInt()
+const val Xd0 = 0x00d0.toInt()
+const val Xd1 = 0x00d1.toInt()
+const val Xd2 = 0x00d2.toInt()
+const val Xd3 = 0x00d3.toInt()
+const val Xd4 = 0x00d4.toInt()
+const val Xd5 = 0x00d5.toInt()
+const val Xd6 = 0x00d6.toInt()
+const val Xd7 = 0x00d7.toInt()
+const val Xd8 = 0x00d8.toInt()
+const val Xd9 = 0x00d9.toInt()
+const val Xda = 0x00da.toInt()
+const val Xdb = 0x00db.toInt()
+const val Xdc = 0x00dc.toInt()
+const val Xdd = 0x00dd.toInt()
+const val Xde = 0x00de.toInt()
+const val Xdf = 0x00df.toInt()
+const val Xe0 = 0x00e0.toInt()
+const val Xe1 = 0x00e1.toInt()
+const val Xe2 = 0x00e2.toInt()
+const val Xe3 = 0x00e3.toInt()
+const val Xe4 = 0x00e4.toInt()
+const val Xe5 = 0x00e5.toInt()
+const val Xe6 = 0x00e6.toInt()
+const val Xe7 = 0x00e7.toInt()
+const val Xe8 = 0x00e8.toInt()
+const val Xe9 = 0x00e9.toInt()
+const val Xea = 0x00ea.toInt()
+const val Xeb = 0x00eb.toInt()
+const val Xec = 0x00ec.toInt()
+const val Xed = 0x00ed.toInt()
+const val Xee = 0x00ee.toInt()
+const val Xef = 0x00ef.toInt()
+const val Xf0 = 0x00f0.toInt()
+const val Xf1 = 0x00f1.toInt()
+const val Xf2 = 0x00f2.toInt()
+const val Xf3 = 0x00f3.toInt()
+const val Xf4 = 0x00f4.toInt()
+const val Xf5 = 0x00f5.toInt()
+const val Xf6 = 0x00f6.toInt()
+const val Xf7 = 0x00f7.toInt()
+const val Xf8 = 0x00f8.toInt()
+const val Xf9 = 0x00f9.toInt()
+const val Xfa = 0x00fa.toInt()
+const val Xfb = 0x00fb.toInt()
+const val Xfc = 0x00fc.toInt()
+const val Xfd = 0x00fd.toInt()
+const val Xfe = 0x00fe.toInt()
+const val Xff = 0x00ff.toInt()
+
+val ASCII: HashMap<Int,Int>
+    get() = hashMapOf<Int,Int>(
+
+    )
+
+val EBCBIC: HashMap<Int, Int>
+    get() = hashMapOf<Int,Int>(
+            0 to 0,
+            1 to 1,
+            2 to 2,
+            3 to 3,
+            4 to 26,
+            5 to 9,
+            6 to 26,
+            7 to 127,
+            8 to 26,
+            9 to 26,
+            10 to 26,
+            11 to 11,
+            12 to 12,
+            13 to 13,
+            14 to 14,
+            15 to 15,
+            16 to 16,
+            17 to 17,
+            18 to 18,
+            19 to 19,
+            20 to 26,
+            21 to 26,
+            22 to 8,
+            23 to 26,
+            24 to 24,
+            25 to 25,
+            26 to 26,
+            27 to 26,
+            28 to 28,
+            29 to 29,
+            30 to 30,
+            31 to 31,
+            32 to 26,
+            33 to 26,
+            34 to 26,
+            35 to 26,
+            36 to 26,
+            37 to 10,
+            38 to 23,
+            39 to 27,
+            40 to 26,
+            41 to 26,
+            42 to 26,
+            43 to 26,
+            44 to 26,
+            45 to 5,
+            46 to 6,
+            47 to 7,
+            48 to 26,
+            49 to 26,
+            50 to 22,
+            51 to 26,
+            52 to 26,
+            53 to 26,
+            54 to 26,
+            55 to 4,
+            56 to 26,
+            57 to 26,
+            58 to 26,
+            59 to 26,
+            60 to 20,
+            61 to 21,
+            62 to 26,
+            63 to 26,
+            64 to 32,
+            65 to 26,
+            66 to 26,
+            67 to 26,
+            68 to 26,
+            69 to 26,
+            70 to 26,
+            71 to 26,
+            72 to 26,
+            73 to 26,
+            74 to 91,
+            75 to 46,
+            76 to 60,
+            77 to 40,
+            78 to 43,
+            79 to 33,
+            80 to 38,
+            81 to 26,
+            82 to 26,
+            83 to 26,
+            84 to 26,
+            85 to 26,
+            86 to 26,
+            87 to 26,
+            88 to 26,
+            89 to 26,
+            90 to 93,
+            91 to 36,
+            92 to 42,
+            93 to 41,
+            94 to 59,
+            95 to 94,
+            96 to 45,
+            97 to 26,
+            98 to 26,
+            99 to 26,
+            100 to 26,
+            101 to 26,
+            102 to 26,
+            103 to 26,
+            104 to 26,
+            105 to 26,
+            106 to 124,
+            107 to 44,
+            108 to 37,
+            109 to 95,
+            110 to 62,
+            111 to 63,
+            112 to 26,
+            113 to 26,
+            114 to 26,
+            115 to 26,
+            116 to 26,
+            117 to 26,
+            118 to 26,
+            119 to 26,
+            120 to 26,
+            121 to 96,
+            122 to 58,
+            123 to 35,
+            124 to 64,
+            125 to 39,
+            126 to 61,
+            127 to 34,
+            128 to 26,
+            129 to 97,
+            130 to 98,
+            131 to 99,
+            132 to 100,
+            133 to 101,
+            134 to 102,
+            135 to 103,
+            136 to 104,
+            137 to 105,
+            138 to 26,
+            139 to 26,
+            140 to 26,
+            141 to 26,
+            142 to 26,
+            143 to 26,
+            144 to 26,
+            145 to 106,
+            146 to 107,
+            147 to 108,
+            148 to 109,
+            149 to 110,
+            150 to 111,
+            151 to 112,
+            152 to 113,
+            153 to 114,
+            154 to 26,
+            155 to 26,
+            156 to 26,
+            157 to 26,
+            158 to 26,
+            159 to 26,
+            160 to 26,
+            161 to 126,
+            162 to 115,
+            163 to 116,
+            164 to 117,
+            165 to 118,
+            166 to 119,
+            167 to 120,
+            168 to 121,
+            169 to 122,
+            170 to 26,
+            171 to 26,
+            172 to 26,
+            173 to 26,
+            174 to 26,
+            175 to 26,
+            176 to 26,
+            177 to 26,
+            178 to 26,
+            179 to 26,
+            180 to 26,
+            181 to 26,
+            182 to 26,
+            183 to 26,
+            184 to 26,
+            185 to 26,
+            186 to 26,
+            187 to 26,
+            188 to 26,
+            189 to 26,
+            190 to 26,
+            191 to 26,
+            192 to 123,
+            193 to 65,
+            194 to 66,
+            195 to 67,
+            196 to 68,
+            197 to 69,
+            198 to 70,
+            199 to 71,
+            200 to 72,
+            201 to 73,
+            202 to 26,
+            203 to 26,
+            204 to 26,
+            205 to 26,
+            206 to 26,
+            207 to 26,
+            208 to 125,
+            209 to 74,
+            210 to 75,
+            211 to 76,
+            212 to 77,
+            213 to 78,
+            214 to 79,
+            215 to 80,
+            216 to 81,
+            217 to 82,
+            218 to 26,
+            219 to 26,
+            220 to 26,
+            221 to 26,
+            222 to 26,
+            223 to 26,
+            224 to 92,
+            225 to 26,
+            226 to 83,
+            227 to 84,
+            228 to 85,
+            229 to 86,
+            230 to 87,
+            231 to 88,
+            232 to 89,
+            233 to 90,
+            234 to 26,
+            235 to 26,
+            236 to 26,
+            237 to 26,
+            238 to 26,
+            239 to 26,
+            240 to 48,
+            241 to 49,
+            242 to 50,
+            243 to 51,
+            244 to 52,
+            245 to 53,
+            246 to 54,
+            247 to 55,
+            248 to 56,
+            249 to 57,
+            250 to 26,
+            251 to 26,
+            252 to 26,
+            253 to 26,
+            254 to 26,
+            255 to 26
+    )
+
